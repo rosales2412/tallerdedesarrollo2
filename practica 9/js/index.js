da0b40188fc03ec33c916c466f13eb7be577c6d5
rosales2412 <111525241+rosales2412@users.noreply.github.com>
2023-04-23 16:16:15 -0600

    Add files via upload

--- practica 9/js/index.js
+++ practica 9/js/index.js
@@ -1,45 +1,46 @@
-let nombreArtista = document.getElementById('nombreArtista');
-let nombreAlbum = document.getElementById('nombreAlbum');
-let fotoArtista = document.getElementById('fotoArtista');
-let listaCanciones = document.getElementById('listaCanciones');
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '82c331f6b9msh56dfd4f13c1594cp101eefjsn6d2154f30e97',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
-};
-fetch('https://spotify23.p.rapidapi.com/albums/?ids=4geCevoFyJ8TvKJdNfSIRb', options)
-	.then(response => response.json())
-	.then(response => {
-        //nombreArtista.innerHTML = response.albums[0].name;
-            console.log(response.albums[0].name);
-            console.log(response.albums[0].artists[0].name);
-            console.log(response.albums[0].images[0].url);
-            console.log(response.albums[0].tracks.items[0].preview_url);
-
-            nombreArtista.innerHTML = response.albums[0].name;
-            nombreAlbum.innerHTML = response.albums[0].artists[0].name;
-            fotoArtista.src = response.albums[0].images[0].url;
-
-            let canciones = response.albums[0].tracks.items;
-            
-            let todasLasCanciones = '';
-            canciones.forEach((cancion,index)=>{
-                let nombreCancion = cancion.name;
-                let cancionUrl = cancion.preview_url;
-                todasLasCanciones += `
-                <div class="col-12 col-md-4 text-center">
-                    <p>${nombreCancion}</p>
-                    <audio controls>
-                        <source src="${cancionUrl}" type="audio/mpeg">
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>`;
-                console.log(nombreCancion);
-                console.log(cancion.preview_url);
-            });
-            listaCanciones.innerHTML = todasLasCanciones;
-        })
-	.catch(err => console.error(err));
+function searchAlbum() {
+    let nombreArtista = document.getElementById('nombreArtista');
+    let nombreAlbum = document.getElementById('nombreAlbum');
+    let fotoArtista = document.getElementById('fotoArtista');
+    let listaCanciones = document.getElementById('listaCanciones');
+    const artistId = document.getElementById('artist-id').value;
+    const url = `https://spotify23.p.rapidapi.com/albums/?ids=${artistId}`;
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '82c331f6b9msh56dfd4f13c1594cp101eefjsn6d2154f30e97',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    
+    
+    fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+        nombreArtista.innerHTML = response.albums[0].artists[0].name;
+        nombreAlbum.innerHTML = response.albums[0].name;
+        fotoArtista.src = response.albums[0].images[0].url;
+    
+        let canciones = response.albums[0].tracks.items;
+        
+        let todasLasCanciones = '';
+        canciones.forEach((cancion,index)=>{
+            let nombreCancion = cancion.name;
+            let cancionUrl = cancion.preview_url;
+            todasLasCanciones += `
+            <div class="col-12 col-md-4 text-center">
+                <p>${nombreCancion}</p>
+                <audio controls>
+                    <source src="${cancionUrl}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            </div>`;
+            console.log(nombreCancion);
+            console.log(cancion.preview_url);
+        });
+        listaCanciones.innerHTML = todasLasCanciones;
+    })
+    .catch(err => console.error(err));
+    
+    }
